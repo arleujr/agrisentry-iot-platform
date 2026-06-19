68aec7bb5dece79c54c94a98b72bb1ce99bbd2f3
Arleu Júnior <arleujr30@gmail.com>
2026-06-19 18:27:59 -0300

    Update api.rs

--- gateway-rust/src/api.rs
+++ gateway-rust/src/api.rs
@@ -3,7 +3,7 @@ use crate::models::SensorPayload;
 use crate::db::DbClient;
 use crate::error::GatewayError;
 
-#[post("/api/v1/telemetry")]
+#[post("/telemetry")]
 pub async fn ingest_telemetry(
     db: web::Data<DbClient>,
     payload: web::Json<SensorPayload>,
